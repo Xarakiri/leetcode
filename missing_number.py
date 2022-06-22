3fc2375a8df31a10592ea8c136f3d50c04cfc758
Xarakiri <rafikov.ds777@gmail.com>
2022-06-22 12:31:45 +0500

    chore: add comment

--- missing_number.py
+++ missing_number.py
@@ -5,5 +5,5 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        sum_ = n * (n+1)//2
+        sum_ = n * (n+1)//2 # сумма арифметической прогрессии
         return sum_ - sum(nums)
